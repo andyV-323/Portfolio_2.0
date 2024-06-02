4830230470c8185d86ede306db0b1d1b7aa1cdec
andyV-323 <40248914+andyV-323@users.noreply.github.com>
2024-06-01 23:16:09 -0700

    Lightning fix

--- src/components/Puffycloud.jsx
+++ src/components/Puffycloud.jsx
@@ -1,45 +1,21 @@
-// components/Puffycloud.js
-import React, { useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
+import React from 'react';
 import { RigidBody, BallCollider } from '@react-three/rapier';
+import { usePuffycloud } from '../hooks/usePuffycloud';
 import { Cloud } from '@react-three/drei';
-import * as THREE from 'three';
-import { random } from 'maath';
 
-const Puffycloud = ({
-  seed,
-  vec = new THREE.Vector3(),
-  isDarkMode,
-  ...props
-}) => {
-  const api = useRef();
-  const light = useRef();
-  const [flash] = useState(
-    () => new random.FlashGen({ count: 10, minDuration: 40, maxDuration: 200 })
-  );
-
-  const contact = (payload) => {
-    if (
-      payload.other.rigidBodyObject.userData?.cloud &&
-      payload.totalForceMagnitude / 1000 > 100
-    ) {
-      flash.burst();
-    }
-  };
-
-  useFrame((state, delta) => {
-    const impulse = flash.update(state.clock.elapsedTime, delta);
-    light.current.intensity = impulse * 15000;
-    api.current?.applyImpulse(
-      vec.copy(api.current.translation()).negate().multiplyScalar(10)
-    );
-  });
+const Puffycloud = React.memo(({ seed, isDarkMode, ...props }) => {
+  const { api, light, flash } = usePuffycloud({ seed, isDarkMode });
 
   return (
     <RigidBody
+      castShadow
       ref={api}
       userData={{ cloud: true }}
-      onContactForce={contact}
+      onContactForce={(payload) =>
+        payload.other.rigidBodyObject.userData?.cloud &&
+        payload.totalForceMagnitude / 1000 > 100 &&
+        flash.burst()
+      }
       linearDamping={4}
       angularDamping={1}
       friction={0.1}
@@ -48,6 +24,7 @@ const Puffycloud = ({
     >
       <BallCollider args={[4]} />
       <Cloud
+        castShadow
         seed={seed}
         fade={30}
         speed={0.1}
@@ -58,6 +35,7 @@ const Puffycloud = ({
         bounds={[4, 3, 1]}
       />
       <Cloud
+        castShadow
         seed={seed + 1}
         fade={30}
         position={[0, 1, 0]}
@@ -70,6 +48,6 @@ const Puffycloud = ({
       <pointLight position={[0, 0, 0.5]} ref={light} color="blue" />
     </RigidBody>
   );
-};
+});
 
 export default Puffycloud;
