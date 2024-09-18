2aad4c0fc965ffd5dce425b28ddb16af2731cf61
AndyV <you@example.com>
2024-09-18 13:05:51 -0700

    ga4

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,8 @@ import { Home, About, Contact } from "./pages";
 import { Projects } from "./pages/Projects";
 import { DarkModeProvider } from "./components/DarkModeContext";
 import ConditionalFooter from "./components/ConditionalFooter"; // Import the new component
-import { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import { initGA, logPageView } from "./analytics";
 
 const App = () => {
-  useEffect(() => {
-    initGA();
-  }, []);
-  const location = useLocation();
-
-  useEffect(() => {
-    logPageView();
-  }, [location]);
   return (
     <main className="bg-slate-300/20">
       <Router>
