65c7b039894dd91b5e90637dbd8de61b6c094408
AndyV <you@example.com>
2024-09-28 12:36:36 -0700

    google fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,8 @@ import { Home, About, Contact } from "./pages";
 import { Projects } from "./pages/Projects";
 import { DarkModeProvider } from "./components/DarkModeContext";
 import ConditionalFooter from "./components/ConditionalFooter"; // Import the new component
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import TagManager from "react-gtm-module";
 
 const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    const tagManagerArgs = {
-      gtmId: "GTM-5LGWK5MS", // Replace with your GTM ID
-    };
-    TagManager.initialize(tagManagerArgs);
-  }, []);
-
-  useEffect(() => {
-    window.dataLayer.push({
-      event: "pageview",
-      page: location.pathname,
-    });
-  }, [location]);
-
   return (
     <main className="bg-slate-300/20">
       <Router>
