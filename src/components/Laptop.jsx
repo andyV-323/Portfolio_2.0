79a83028c5952dc47de8a117a75cd7fbb843d9ca
AndyV <you@example.com>
2025-07-01 17:07:56 -0700

    Added moon and laptop animation

--- src/components/Laptop.jsx
+++ src/components/Laptop.jsx
@@ -1,36 +1,51 @@
-import React, { useState } from 'react';
-import { Macbook } from '../models';
+import React, { useState } from "react";
+import { Macbook } from "../models";
 
-const Laptop = () => {
-  const [currentAnimation, setCurrentAnimation] = useState('Idle');
-  const [macAnimation, setMacAnimation] = useState(false);
+const Laptop = ({ onPointerOver, onPointerOut }) => {
+	const [macAnimation, setMacAnimation] = useState("Idle");
 
-  const adjustMacbookForScreenSize = () => {
-    let screenScale, screenPosition, screenRotation;
+	const adjustMacbookForScreenSize = () => {
+		let screenScale, screenPosition;
 
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-      screenPosition = [0, -10.5, 6.5];
-    } else {
-      screenScale = [1, 1, 1];
-      screenPosition = [0, -10.5, 6.5];
-    }
+		if (window.innerWidth < 768) {
+			screenScale = [0.9, 0.9, 0.9];
+			screenPosition = [0, -10.5, 6.5];
+		} else {
+			screenScale = [1, 1, 1];
+			screenPosition = [0, -10.5, 6.5];
+		}
 
-    return [screenScale, screenPosition, screenRotation];
-  };
-  const [macbookScale, macbookPosition] = adjustMacbookForScreenSize();
+		return [screenScale, screenPosition];
+	};
 
-  const handleMacClick = () => {
-    setMacAnimation((prev) => (prev ? '' : 'Animation'));
-  };
-  return (
-    <Macbook
-      position={macbookPosition}
-      scale={macbookScale}
-      macAnimation={currentAnimation}
-      onClick={handleMacClick}
-    />
-  );
+	const [macbookScale, macbookPosition] = adjustMacbookForScreenSize();
+
+	const handleMacClick = () => {
+		setMacAnimation((prev) => (prev === "Idle" ? "Animation" : "Idle"));
+	};
+
+	return (
+		<>
+			<mesh
+				onClick={handleMacClick}
+				position={macbookPosition}>
+				<boxGeometry args={[3, 1, 1.5]} />
+				<meshBasicMaterial
+					transparent
+					opacity={1}
+					visible={false}
+				/>
+			</mesh>
+			<Macbook
+				position={macbookPosition}
+				scale={macbookScale}
+				macAnimation={macAnimation}
+				onPointerOver={onPointerOver}
+				onPointerOut={onPointerOut}
+				castShadow
+			/>
+		</>
+	);
 };
 
 export default Laptop;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,138 +1,149 @@
 // Home.js
-import * as THREE from 'three';
-import { useDarkMode } from '../components/DarkModeContext';
-import { useRef, useMemo } from 'react';
-import { Canvas } from '@react-three/fiber';
+import * as THREE from "three";
+import { useDarkMode } from "../components/DarkModeContext";
+import { useRef, useMemo } from "react";
+import { Canvas } from "@react-three/fiber";
 import {
-  Clouds,
-  Cloud,
-  OrbitControls,
-  ContactShadows,
-  Environment,
-} from '@react-three/drei';
-import { CuboidCollider, Physics } from '@react-three/rapier';
+	Clouds,
+	Cloud,
+	OrbitControls,
+	ContactShadows,
+	Environment,
+} from "@react-three/drei";
+import { CuboidCollider, Physics } from "@react-three/rapier";
 import {
-  Fenrir,
-  Andy,
-  Laptop,
-  Luna,
-  PerspectiveCam,
-  Lighting,
-  Overlay,
-  Puffycloud,
-  Pointer,
-} from '../components';
-import { Context } from '../components/Context';
+	Fenrir,
+	Andy,
+	Laptop,
+	Luna,
+	PerspectiveCam,
+	Lighting,
+	Overlay,
+	Puffycloud,
+	Pointer,
+} from "../components";
+import { Context } from "../components/Context";
 
 export default function Home() {
-  const { isDarkMode } = useDarkMode();
-  const shakeRef = useRef();
-  const contextValue = useMemo(() => ({ shakeRef }), [isDarkMode]);
+	const { isDarkMode } = useDarkMode();
+	const shakeRef = useRef();
+	const contextValue = useMemo(() => ({ shakeRef }), [isDarkMode]);
 
-  const handlePointerOver = () => {
-    document.querySelector('.canvas-container').classList.add('pointer-cursor');
-  };
+	const handlePointerOver = () => {
+		document.querySelector(".canvas-container").classList.add("pointer-cursor");
+	};
 
-  const handlePointerOut = () => {
-    document
-      .querySelector('.canvas-container')
-      .classList.remove('pointer-cursor');
-  };
+	const handlePointerOut = () => {
+		document
+			.querySelector(".canvas-container")
+			.classList.remove("pointer-cursor");
+	};
 
-  return (
-    <section className="w-full h-screen relative">
-      <Canvas className="canvas-container" shadows>
-        <ambientLight intensity={Math.PI / 2} />
-        <PerspectiveCam />
-        <Lighting />
-        <Context.Provider value={contextValue}>
-          <Clouds
-            limit={400}
-            material={THREE.MeshLambertMaterial}
-            castShadow={true}
-            receiveShadow
-          >
-            <Physics gravity={[0, 0, 0]}>
-              <Pointer />
-              <Puffycloud
-                seed={10}
-                position={[50, 0, 0]}
-                castShadow
-                isDarkMode={isDarkMode}
-              />
-              <Puffycloud
-                seed={20}
-                position={[0, 50, 0]}
-                castShadow
-                isDarkMode={isDarkMode}
-              />
-              <Puffycloud
-                seed={30}
-                position={[50, 0, 50]}
-                castShadow
-                isDarkMode={isDarkMode}
-              />
-              <Puffycloud
-                seed={40}
-                position={[0, 0, -50]}
-                castShadow
-                isDarkMode={isDarkMode}
-              />
-              <CuboidCollider
-                position={[0, -5, 0]}
-                args={[400, 10, 400]}
-                castShadow
-              />
-            </Physics>
-          </Clouds>
-        </Context.Provider>
+	return (
+		<section className='w-full h-screen relative'>
+			<Canvas
+				className='canvas-container'
+				shadows>
+				<ambientLight intensity={Math.PI / 2} />
+				<PerspectiveCam />
+				<Lighting />
+				<Context.Provider value={contextValue}>
+					<Clouds
+						limit={400}
+						material={THREE.MeshLambertMaterial}
+						castShadow={true}
+						receiveShadow>
+						<Physics gravity={[0, 0, 0]}>
+							<Pointer />
+							<Puffycloud
+								seed={10}
+								position={[50, 0, 0]}
+								castShadow
+								isDarkMode={isDarkMode}
+							/>
+							<Puffycloud
+								seed={20}
+								position={[0, 50, 0]}
+								castShadow
+								isDarkMode={isDarkMode}
+							/>
+							<Puffycloud
+								seed={30}
+								position={[50, 0, 50]}
+								castShadow
+								isDarkMode={isDarkMode}
+							/>
+							<Puffycloud
+								seed={40}
+								position={[0, 0, -50]}
+								castShadow
+								isDarkMode={isDarkMode}
+							/>
+							<CuboidCollider
+								position={[0, -5, 0]}
+								args={[400, 10, 400]}
+								castShadow
+							/>
+						</Physics>
+					</Clouds>
+				</Context.Provider>
 
-        <OrbitControls
-          makeDefault
-          autoRotate={false}
-          enableZoom={false}
-          enablePan={false}
-          minPolarAngle={Math.PI / 1.7}
-          maxPolarAngle={Math.PI / 1.7}
-        />
+				<OrbitControls
+					makeDefault
+					autoRotate={false}
+					enableZoom={false}
+					enablePan={false}
+					minPolarAngle={Math.PI / 1.7}
+					maxPolarAngle={Math.PI / 1.7}
+				/>
 
-        <mesh receiveShadow={true} castShadow={true}>
-          {isDarkMode ? (
-            <>
-              <Fenrir
-                onPointerOver={handlePointerOver}
-                onPointerOut={handlePointerOut}
-              />
-            </>
-          ) : (
-            <>
-              <Andy
-                onPointerOver={handlePointerOver}
-                onPointerOut={handlePointerOut}
-              />
-            </>
-          )}
-        </mesh>
+				<mesh
+					receiveShadow={true}
+					castShadow={true}>
+					{isDarkMode ? (
+						<>
+							<Fenrir
+								onPointerOver={handlePointerOver}
+								onPointerOut={handlePointerOut}
+							/>
+							<Luna />
+							<Laptop
+								onPointerOver={handlePointerOver}
+								onPointerOut={handlePointerOut}
+							/>
+						</>
+					) : (
+						<>
+							<Andy
+								onPointerOver={handlePointerOver}
+								onPointerOut={handlePointerOut}
+							/>
+							<Laptop
+								onPointerOver={handlePointerOver}
+								onPointerOut={handlePointerOut}
+							/>
+						</>
+					)}
+				</mesh>
 
-        <mesh
-          receiveShadow={true}
-          position={[0, -10.5, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-        >
-          <planeGeometry args={[500, 500]} />
-          <meshStandardMaterial color={isDarkMode ? 'black' : 'white'} />
-        </mesh>
+				<mesh
+					receiveShadow={true}
+					position={[0, -10.5, 0]}
+					rotation={[-Math.PI / 2, 0, 0]}>
+					<planeGeometry args={[500, 500]} />
+					<meshStandardMaterial color={isDarkMode ? "black" : "white"} />
+				</mesh>
 
-        <ContactShadows
-          opacity={0.25}
-          color="black"
-          position={[0, -10, 0]}
-          scale={50}
-          blur={2.5}
-          far={40}
-        />
-        <Overlay />
-      </Canvas>
-    </section>
-  );
+				<ContactShadows
+					opacity={0.25}
+					color='black'
+					position={[0, -10, 0]}
+					scale={50}
+					blur={2.5}
+					far={40}
+				/>
+				<Overlay />
+			</Canvas>
+		</section>
+	);
 }
